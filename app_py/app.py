55898ef6c88feb89403819cea2f0fd6e873fe857
BolaWagdy <bolawagdy05@gmail.com>
2024-07-20 14:54:22 +0300

    File

--- app_py/app.py
+++ app_py/app.py
@@ -3,11 +3,9 @@ from io import BytesIO
 import qrcode
 import json
 import datetime
-import requests  # Library for making HTTP requests
 
 app = Flask(__name__)
 
-# Function to fetch location based on IP address
 def get_location(ip_address):
     try:
         response = requests.get(f'http://ipinfo.io/{ip_address}/json')
@@ -32,28 +30,27 @@ def index():
         buffer.seek(0)
         ip_address = request.remote_addr
         location = get_location(ip_address)
-        log_visit(ip_address, text, location)  # Log each QR code generation with location
+        log_visit(ip_address, text, location) 
         return send_file(buffer, mimetype='image/png', as_attachment=True, download_name='qrcode.png')
     return render_template('index.html')
 
-# Function to log visits in a JSON file
 def log_visit(ip_address, text, location):
     log_entry = {
-        'timestamp': datetime.datetime.now().isoformat(),  # Current time in ISO format
-        'ip_address': ip_address,  # Visitor's IP address
-        'text': text,  # Text used to generate the QR code
-        'location': location  # Location data (city, region, country)
+        'timestamp': datetime.datetime.now().isoformat(),
+        'ip_address': ip_address, 
+        'text': text, 
+        'location': location 
     }
     try:
         with open('db/users_visits.json', 'r') as f:
-            data = json.load(f)  # Load existing log data
+            data = json.load(f) 
     except FileNotFoundError:
-        data = []  # If file not found, start with an empty list
+        data = []  
     
-    data.append(log_entry)  # Add new log entry
+    data.append(log_entry)  
     
     with open('db/users_visits.json', 'w') as f:
-        json.dump(data, f, indent=4)  # Write updated data back to the JSON file
+        json.dump(data, f, indent=4) 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8081)  # Run the Flask app
+    app.run(debug=True, host='0.0.0.0', port=8084)
--- app_py/db/analyze.py
+++ app_py/db/analyze.py
@@ -3,12 +3,12 @@ import json
 def analyze_visits(log_file='users_visits.json'):
     try:
         with open(log_file, 'r') as f:
-            data = json.load(f)  # Load log data
-        ip_addresses = {entry['ip_address'] for entry in data}  # Extract unique IP addresses
-        unique_visits = len(ip_addresses)  # Count unique visits
+            data = json.load(f)
+        ip_addresses = {entry['ip_address'] for entry in data} 
+        unique_visits = len(ip_addresses)  
         print(f'Unique visits: {unique_visits}')
     except FileNotFoundError:
-        print('Log file not found.')  # Handle missing log file
+        print('Log file not found.') 
 
 if __name__ == "__main__":
-    analyze_visits()  # Run analysis when script is executed
+    analyze_visits() 
