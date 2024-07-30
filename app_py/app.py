7758abf22d541b20fd4910575398c6cdd77f8273
BolaWagdy <bolawagdy05@gmail.com>
2024-07-31 00:35:28 +0300

    Edited

--- app_py/app.py
+++ app_py/app.py
@@ -53,4 +53,4 @@ def log_visit(ip_address, text, location):
         json.dump(data, f, indent=4) 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8084)
+    app.run(debug=True, host='0.0.0.0', port=8080)
